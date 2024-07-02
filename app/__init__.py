a9bc851a7d415bb26a685a6818eb341649e5daa4
Nitin K <kendre.nitin2000@gmail.com>
2024-07-02 11:51:28 +0530

    added markdown support to editor. and login manager in app.

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 from flask_login import LoginManager
+from flask_pagedown import PageDown
 
 bootstrap = Bootstrap()
 moment = Moment()
@@ -12,6 +13,8 @@ mail = Mail()
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
+
+pagedown = PageDown()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -23,6 +26,7 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    pagedown.init_app(app)
 
     # main blueprint
     from .main import main as main_blueprint
