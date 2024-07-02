9fee735970a4fcfc53f4c78453f9b19c6b685d17
Nitin K <kendre.nitin2000@gmail.com>
2024-07-02 12:16:49 +0530

    create one bat file to set all environment variables.

---
+++ set_environment_variables.bat
@@ -0,0 +1,8 @@
+@echo off
+set FLASK_APP=nkblog.py
+set MAIL_USERNAME=example@gmail.com
+set MAIL_PASSWORD=password
+set NKBLOG_ADMIN=nsk@nkblog.com
+set DEBUG=1
+set NLBLOG_POSTS_PER_PAGE=20
+pause
