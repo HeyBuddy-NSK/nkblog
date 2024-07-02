cee5e4a35baf8554dacee6b4f0fd852b0e8ea755
Nitin K <kendre.nitin2000@gmail.com>
2024-07-02 11:56:36 +0530

    added logic to show posts on home page and user profile. also added logic to show post creator/editor on home page.

--- app/main/views.py
+++ app/main/views.py
@@ -1,44 +1,61 @@
 from datetime import datetime
-from flask import render_template, session, redirect, url_for, flash
+from flask import render_template, session, redirect, url_for, flash, request
 from . import main
 from .. import db
-from .forms import NameForm, EditProfileForm, EditProfileAdminForm
-from ..models import User, Role
+from .forms import NameForm, EditProfileForm, EditProfileAdminForm, PostForm
+from ..models import User, Role, Permission, Post
 from ..email import send_mail
-from flask import current_app as app
+from flask import current_app as app, abort
 from flask_login import login_required, current_user
 from ..decorators import admin_required
 
 @main.route('/',methods=['GET','POST'])
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            db.session.commit()
-            session['known'] = False
-            if app.config['NKBLOG_ADMIN']:
-                send_mail(app.config['NKBLOG_ADMIN'],'New User', 'Mail/new_user',user=user)
-                print("mail sent successfully.")
-            else:
-                print("--no admin mail--")
+    # form = NameForm()
+    # if form.validate_on_submit():
+    #     user = User.query.filter_by(username=form.name.data).first()
+    #     if user is None:
+    #         user = User(username=form.name.data)
+    #         db.session.add(user)
+    #         db.session.commit()
+    #         session['known'] = False
+    #         if app.config['NKBLOG_ADMIN']:
+    #             send_mail(app.config['NKBLOG_ADMIN'],'New User', 'Mail/new_user',user=user)
+    #             print("mail sent successfully.")
+    #         else:
+    #             print("--no admin mail--")
 
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        form.name.data = ''
+    #     else:
+    #         session['known'] = True
+    #     session['name'] = form.name.data
+    #     form.name.data = ''
+    
+    form = PostForm()
+    if current_user.can(Permission.WRITE) and form.validate_on_submit():
+        post = Post(body=form.body.data, author=current_user._get_current_object())
+        db.session.add(post)
+        db.session.commit()
 
         return redirect(url_for('.index'))
     
-    return render_template('index.html',form=form,name=session.get('name'),
-                           known=session.get('known',False),current_time=datetime.utcnow())
+    # posts = Post.query.order_by(Post.timestamp.desc()).all()
+    # paginating
+    page = request.args.get('page',1,type=int)
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page=page, per_page=int(app.config['NLBLOG_POSTS_PER_PAGE']),error_out=False
+    )
+    posts = pagination.items
+
+    return render_template('index.html',form=form, posts=posts,Permission=Permission, pagination=pagination)
 
 @main.route('/user/<username>')
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    return render_template('user.html',user=user)
+    if user is None:
+        abort(404)
+    
+    posts = user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('user.html',user=user,posts=posts)
 
 
 # function to edit profile of user
@@ -87,3 +104,31 @@ def edit_profile_admin(id):
     form.about_me.data = user.about_me
     return render_template('edit_profile.html',form=form,user=user)
 
+
+# function to generate permanent link for posts.
+@main.route('/post/<int:id>')
+def post(id):
+    post = Post.query.get_or_404(id)
+    return render_template('post.html',posts=[post])
+
+
+# function to edit posts in db and render the template to edit the post.
+@main.route('/edit/<int:id>',methods=['GET','POST'])
+@login_required
+def edit(id):
+    post = Post.query.get_or_404(id)
+    if current_user != post.author and not current_user.can(Permission.ADMIN):
+        abort(403)
+
+    form = PostForm()
+
+    if form.validate_on_submit():
+        post.body = form.body.data
+        db.session.add(post)
+        db.session.commit()
+
+        flash('The post has been updated')
+        return redirect(url_for('.post',id=post.id))
+    
+    form.body.data = post.body
+    return render_template('edit_post.html',form=form)
