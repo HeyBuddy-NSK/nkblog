25cffc9f15916c974047326b3251d1a80daa498e
Nitin K <kendre.nitin2000@gmail.com>
2024-07-02 12:10:45 +0530

    added a function to perform unit test on the app.

--- nkblog.py
+++ nkblog.py
@@ -1,13 +1,20 @@
 import os
 from app import create_app, db
-from app.models import User
+from app.models import User, Role
 from flask_migrate import Migrate
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
 
 with app.app_context():
     db.create_all()
+    Role.insert_roles()
 
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User)
+
+@app.cli.command()
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
