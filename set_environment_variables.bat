c475b9caf060d57523f8aab5effdf8a8b6814ae0
Nitin K <kendre.nitin2000@gmail.com>
2024-07-05 16:26:55 +0530

    adjusted code to take input from cmd directly.

--- set_environment_variables.bat
+++ set_environment_variables.bat
@@ -1,8 +1,9 @@
+:main
 @echo off
-set FLASK_APP=nkblog.py
-set MAIL_USERNAME=example@gmail.com
-set MAIL_PASSWORD=password
-set NKBLOG_ADMIN=nsk@nkblog.com
-set DEBUG=1
-set NLBLOG_POSTS_PER_PAGE=20
+set /p FLASK_APP= Enter your main file name :: 
+set /p MAIL_USERNAME= Enter your mail id :: 
+set /p MAIL_PASSWORD= Enter password for mail id :: 
+set /p NKBLOG_ADMIN= Enter mail id of admin :: 
+set /p DEBUG= Do you want debug on ( 1 or 0) :: 
+set /p NKBLOG_POSTS_PER_PAGE= Enter No of posts per page :: 
 pause
