292bcc5d4a8f21d175af4086232cf4a15a85b77e
Nitin K <kendre.nitin2000@gmail.com>
2024-07-09 23:33:53 +0530

    registered api blueprint in the app.

--- app/__init__.py
+++ app/__init__.py
@@ -36,4 +36,8 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix='/auth')
 
+    # api blueprint
+    from .api import api as api_blueprint
+    app.register_blueprint(api_blueprint,url_prefix='/api/v1')
+
     return app
