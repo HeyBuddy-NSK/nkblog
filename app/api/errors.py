3bb3e28f8f68a688a0d45d5e85f48cd286f36050
Nitin K <kendre.nitin2000@gmail.com>
2024-07-09 23:48:46 +0530

    added 404 error handler with http content negotiation

---
+++ app/api/errors.py
@@ -0,0 +1,11 @@
+from . import api
+from flask import request, jsonify, render_template
+
+@api.app_errorhandler(404)
+def page_not_found(e):
+    if request.accept_mimetypes.accept_json and not request.accept_mimetypes.accept_html:
+        response = jsonify({'error':'not found'})
+        response.status_code = 404
+        return response
+    
+    return render_template('404.html'),404
