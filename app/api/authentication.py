1b848747a070daf802aa4c4fc511f0be2e725c9c
Nitin K <kendre.nitin2000@gmail.com>
2024-07-10 14:46:59 +0530

    updated verify_password method so it can use token generated or password.

--- app/api/authentication.py
+++ app/api/authentication.py
@@ -5,20 +5,43 @@ from app.models import User
 from flask import g
 from .errors import unauthorized, forbidden
 
-@basic_auth.verify_password
-def verify_password(email,password):
-    """
-    Verifies the password of user.
-    """
-    if email == '':
-        return False
+# @basic_auth.verify_password
+# def verify_password(email,password):
+#     """
+#     Verifies the password of user.
+#     """
+#     if email == '':
+#         return False
     
-    user = User.query.filter_by(email = email).first()
+#     user = User.query.filter_by(email = email).first()
+#     if not user:
+#         return False
+#     g.current_user = user
+    
+#     return user.verify_password(password)
+
+# new method using token for authentication.
+@basic_auth.verify_password
+def verify_password(email_or_token,password):
+    """
+    verifies the user.
+    """
+    if email_or_token == '':
+        return False
+    if password == '':
+        g.current_user = User.verify_auth_token(email_or_token)
+        g.token_used = True
+
+        return g.current_user is not None
+    
+    # if email and password both are there.
+    user = User.query.filter_by(email=email_or_token).first()
     if not user:
         return False
-    g.current_user = user
     
-    return user.verify_password(password)
+    g.current_user = user
+    g.token_used = False
+    return User.verify_password(password)
 
 # HTTPauth error handler
 @basic_auth.error_handler
