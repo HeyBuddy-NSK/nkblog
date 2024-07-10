bdcfe9f4ffc62ba90441145ddaa0298f219169aa
Nitin K <kendre.nitin2000@gmail.com>
2024-07-10 10:33:36 +0530

    added method to handle unauthorized user error and added structure of get_posts method.

--- app/api/authentication.py
+++ app/api/authentication.py
@@ -1,9 +1,11 @@
 from flask_httpauth import HTTPBasicAuth
-auth = HTTPBasicAuth()
+from . import api
+basic_auth = HTTPBasicAuth()
 from app.models import User
 from flask import g
+from .errors import unauthorized
 
-@auth.verify_password
+@basic_auth.verify_password
 def verify_password(email,password):
     if email == '':
         return False
@@ -14,3 +16,14 @@ def verify_password(email,password):
     g.current_user = user
     
     return user.verify_password(password)
+
+# HTTPauth error handler
+from .errors import unauthorized
+@basic_auth.error_handler
+def auth_error():
+    return unauthorized('Invalid Credentials')
+
+@api.route('/posts/')
+@basic_auth.login_required
+def get_posts():
+    pass
