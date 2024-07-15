3df97d4f99fbb70fd275140fe5fb1a9e23e2e1b6
Nitin K <kendre.nitin2000@gmail.com>
2024-07-15 09:14:39 +0530

    updated importing statement to import decorators.

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 api = Blueprint('api',__name__)
 
-from . import users, posts, comments, errors, authentication
+from . import users, posts, comments, errors, authentication, decorators
