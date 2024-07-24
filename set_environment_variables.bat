6472983ed94615c455d45cbd62eaee3e04c1ff3e
Nitin K <kendre.nitin2000@gmail.com>
2024-07-24 12:49:16 +0530

    removed some logical error from bat file.

--- set_environment_variables.bat
+++ set_environment_variables.bat
@@ -6,6 +6,6 @@ set /p MAIL_PASSWORD= Enter password for mail id ::
 set /p NKBLOG_ADMIN= Enter mail id of admin :: 
 set /p FLASK_DEBUG= Do you want debug on ( 1 or 0) :: 
 set /p NKBLOG_POSTS_PER_PAGE= Enter No of posts per page :: 
-set /p NKBLOG_COMMENTS_PER_PAGE = Enter No of comments per page :: 
-set /p FLASK_COVERAGE = Do you want to coverage (1 or 0) ::  
+set /p NKBLOG_COMMENTS_PER_PAGE= Enter No of comments per page :: 
+set /p FLASK_COVERAGE= Do you want to coverage (1 or 0) ::  
 pause
