62c2b6b48030ae60d5a73f5c2cc7bb80bb57c1d7
Nitin K <kendre.nitin2000@gmail.com>
2024-07-25 10:06:08 +0530

    added code to send mail whenever some critical error occurs in application.

--- config.py
+++ config.py
@@ -51,6 +51,32 @@ class TestingConfig(Config):
     
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+
+    # sending mail if any error occurs
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+
+        # email to admin
+        import logging
+        from logging.handlers import SMTPHandler
+        credentials = None
+        secure = None
+
+        if getattr(cls, 'MAIL_USERNAME', None) is not None:
+            credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+            if getattr(cls, 'MAIL_USE_TLS', None):
+                secure()
+        
+        mail_handler = SMTPHandler(mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
+                                   fromaddr=cls.NKBLOG_MAIL_SENDER,
+                                   toaddrs=[cls.NKBLOG_ADMIN],
+                                   subject=cls.NKBLOG_MAIL_SUBJECT_PREFIX+'Application Error',
+                                   credentials=credentials,
+                                   secure=secure)
+        
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
     
 
 config = {
